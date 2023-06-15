64aebcbee616d8dab2adb33bdaf291eb99cbc0ca
Maike Schröer <maikeschroeer@gmx.de>
2023-06-15 14:03:07 +0200

    generate models,seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name: 'DeGraaf', address: 'Connewitzer Strasse,Leipzig', phone_number: '37896', category: 'belgian')
+Restaurant.create(name: 'Piccolino', address: 'Nordendstrasse, München', phone_number: '67231', category: 'italian')
+Restaurant.create(name: 'Sushisushi', address: 'Amalienstrasse, München', phone_number: '98361', category: 'japanese')
+Restaurant.create(name: 'MiuMiu', address: 'Blumenauer Strasse, München', phone_number: '42163', category: 'chinese')
+Restaurant.create(name: 'Dompierre', address: 'Marienplatz, München', phone_number: '21648', category: 'french')
